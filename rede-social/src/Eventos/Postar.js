7e654c7dc60808a599d6a5797aff2eb45e5bd6da
AlanVieira <alanvieira30@outlook.com>
2023-05-26 16:51:49 -0300

    att

--- rede-social/src/Eventos/Postar.js
+++ rede-social/src/Eventos/Postar.js
@@ -1,9 +1,18 @@
 
 
+import { useState } from "react"
 import styles from "./Postar.module.css"
 
 
 function Postar (){
+    const [post, setValue] = useState()
+ function posted(e) {
+     e.preventDefault()
+    setValue( e.target.elements.inputtext.value)
+    
+}
+  
+    
 
 
 
@@ -13,7 +22,7 @@ return(
 
 <div className={styles.container}>
     <div className={styles.perfilpost}  >
-        <svg className={styles.perfilfeed} xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+        <svg className={styles.perfilfeed} ne xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
     
@@ -21,9 +30,9 @@ return(
             </div>
     
     
-                <form action="">
+                <form onSubmit={posted}>
                     <div className={styles.inputpost}>
-                        <input className={styles.csspost} name="inputtext" id="post" type="text" placeholder="No que você está pensando ?">
+                        <input className={styles.csspost} name="inputtext" id="post" type="text" placeholder="No que você está pensando ?"  >
                         </input>
                     </div>
                     <div className={styles.inputbotao}>
@@ -41,7 +50,13 @@ return(
 
 <div className={styles.feed}>
 <h2>Feed de notícias</h2>
-<p className={styles.res} id="res">ALELECLECLECLECLEC</p>
+<div className={styles.divres}>
+    <p className={styles.res} id="res">{post}</p>
+    <div className={styles.botoes}>
+        <button>Compartilhar</button><button>Curtir</button>
+        <button>Excluir</button>
+    </div>
+</div>
 
 </div>
 </div>
